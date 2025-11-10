767a278119cc2477e141e80150202fa1eff945e4
Cayo <k2@memyselfandi.com>
2025-11-10 14:56:13 +0100

    vault

--- main.tf
+++ main.tf
@@ -12,14 +12,6 @@ terraform {
 }
 
 provider "vault" {
-  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-  skip_child_token = true
-  address          = var.tfc_vault_dynamic_credentials.default.address
-  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
-
-  auth_login_token_file {
-    filename = var.tfc_vault_dynamic_credentials.default.token_filename
-  }
 }
 
 data "vault_kv_secret_v2" "mysecret" {
